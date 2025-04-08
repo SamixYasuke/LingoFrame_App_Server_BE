9feca4312bad388d469804ed2daee643973d2ecb
SamixYasuke <samueladekolu4@gmail.com>
2025-04-08 13:34:00 +0100

    refactor: update CORS configuration to use a dynamic origin callback

--- src/app.ts
+++ src/app.ts
@@ -16,10 +16,20 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: [
-      "http://localhost:3000",
-      "https://lingoframe-landing-page.vercel.app",
-    ],
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        "http://localhost:3000",
+        "https://lingoframe-landing-page.vercel.app",
+      ];
+      console.log("Request Origin:", origin);
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
   })
 );
